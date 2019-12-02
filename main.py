b0c417792b745654746a01965f6584942ea82630
Sean Higgins <shiggins@luc.edu>
2019-12-02 13:04:43 -0600

    change runset order for testing

--- main.py
+++ main.py
@@ -80,6 +80,7 @@ if __name__ == "__main__":
 	# Serial
 	for row in run_set1:
 		run_knn(row[0], row[1], row[2], False, 1)
+		run_knn(row[0], row[1], row[2], True, 50)
 
 	for row in run_set2:
 		run_knn(row[0], row[1], row[2], False, 1)
