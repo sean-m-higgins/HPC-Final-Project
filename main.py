05e7c601101d0901d3fc6130d76b86da064de759
Sean Higgins <shiggins@luc.edu>
2019-12-09 12:24:41 -0600

    fix

--- main.py
+++ main.py
@@ -68,11 +68,11 @@ if __name__ == "__main__":
 	# for row in run_set2:
 	# 	run_knn(row[0], row[1], row[2], False, 1)
 
-	# run_knn(1000, 40000, 10, False, 1)
+	# run_knn(1000, 10000, 10, False, 1)
 	# print("Done")
 
-	# for row in run_set3:
-	# 	run_knn(row[0], row[1], row[2], False, 1)
+	for row in run_set3:
+		run_knn(row[0], row[1], row[2], False, 1)
 
 	# print("Done")
 
@@ -85,9 +85,9 @@ if __name__ == "__main__":
 	# for row in run_set2:
 	# 	run_knn(row[0], row[1], row[2], True, 32)
 
-	run_knn(1000, 10000, 10, True, 32)
+	# run_knn(1000, 10000, 10, True, 32)
 
-	print("Done")
+	# print("Done")
 
 	# for row in run_set3:
 	# 	run_knn(row[0], row[1], row[2], True, 32)
