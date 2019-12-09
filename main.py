05aef05d33dbb886c83e44f06a246bf412b5f142
Sean Higgins <shiggins@luc.edu>
2019-12-09 12:16:33 -0600

    fix

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ if __name__ == "__main__":
 	# columns
 	run_set2 = [ [1000, 4, 10], [1000, 40, 10], [1000, 400, 10], [1000, 4000, 10] ]#, [1000, 40000, 10] ]
 	# k neighbors
-	run_set3 = [ [1000, 4, 10], [1000, 4, 100], [1000, 4, 200], [1000, 4, 400], [1000, 4, 800] ]
+	run_set3 = [ [1000, 4, 10], [1000, 4, 100], [1000, 4, 200], [1000, 4, 400], [1000, 4, 600] ]
 	# processes
 	run_set4 = [[2500, 4, 5, 8], [2500, 4, 5, 16], [2500, 4, 5, 32], [2500, 4, 5, 64], [2500, 4, 5, 128], [2500, 4, 5, 272] ]
 	
@@ -77,15 +77,15 @@ if __name__ == "__main__":
 	# print("Done")
 
 	# # Parallel
-	for row in run_set1:
-		run_knn(row[0], row[1], row[2], True, 32)
+	# for row in run_set1:
+	# 	run_knn(row[0], row[1], row[2], True, 32)
 
-	print("Done")
+	# print("Done")
 
-	for row in run_set2:
-		run_knn(row[0], row[1], row[2], True, 32)
+	# for row in run_set2:
+	# 	run_knn(row[0], row[1], row[2], True, 32)
 
-	print("Done")
+	# print("Done")
 
 	for row in run_set3:
 		run_knn(row[0], row[1], row[2], True, 32)
